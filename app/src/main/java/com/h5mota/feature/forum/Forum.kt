50377bbe517e369e6c3f9f49fb379bd32f0a99d6
tocque <tocque@sina.com>
2024-08-04 00:04:03 +0800

    feat: backup domain

--- app/src/main/java/com/h5mota/feature/forum/Forum.kt
+++ app/src/main/java/com/h5mota/feature/forum/Forum.kt
@@ -5,8 +5,8 @@ import androidx.navigation.compose.composable
 
 const val forumNavigationRoute = "forum_route"
 
-fun NavGraphBuilder.forumScreen(onUrlLoaded: ((String?) -> Unit)? = null) {
+fun NavGraphBuilder.forumScreen(url: String, onUrlLoaded: ((String?) -> Unit)? = null) {
     composable(route = forumNavigationRoute) {
-        ForumRoute(onUrlLoaded = onUrlLoaded)
+        ForumRoute(url, onUrlLoaded = onUrlLoaded)
     }
 }
